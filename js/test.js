6ff6ae276d3ff3e1a233232251faf8c477a51280
ssx88 <admin@web88.ru>
2022-11-02 01:13:06 +0300

    fir

--- js/test.js
+++ js/test.js
@@ -1,14 +1,14 @@
+const lines = 5;
 let result = '';
 
-const value = 15;
-
-for (let i = 1; i < value; i++) {
-
-    for (let j = 0; j < i; j++){
-        result += '*';
+for (let i = 0; i <= lines; i++) {
+    for (let j = 0; j < lines - i; j++) {
+        result += " ";
     }
-
-    result += '\n';
+    for (let j = 0; j < 2 * i + 1; j++) {
+        result += "*";
+    }
+    result += "\n";
 }
 
 console.log(result);
